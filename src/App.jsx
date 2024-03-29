c0c3efee21b01d40c32dcea607c1c95708dc76fe
Arron-B <arronmail@yahoo.com>
2024-03-29 13:31:35 +0000

    mobile hamburger nav and dark mode implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Nav from "./components/Nav";
+import "@theme-toggles/react/css/Classic.css";
+import { Classic } from "@theme-toggles/react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [isDark, setIsDark] = useState(true);
+	const [isNavOpen, setIsNavOpen] = useState(false);
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<div className={"h-dvh w-vw px-4 py-6" + (isDark ? " dark" : "")}>
+				<Nav
+					isNavOpen={isNavOpen}
+					setIsNavOpen={setIsNavOpen}
+				/>
+
+				<Classic
+					className={
+						"top-4 right-4 absolute text-4xl" + (isNavOpen ? " hidden" : "")
+					}
+					toggled={isDark}
+					toggle={setIsDark}
+				/>
+				<h1 className={isNavOpen ? " hidden" : ""}>Arron Bent</h1>
+			</div>
 		</>
 	);
 }
