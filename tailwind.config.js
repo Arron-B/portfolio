e9d182c78cb691169eaf6437bfbf02d8c615c457
Arron-B <arronmail@yahoo.com>
2024-04-05 13:57:21 +0100

    set custom breakpoints in tailwind theme and font-sizes for each breakpoint in CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		screens: {
+			pt: "280px", // begins portrait viewports
+			pt1: "320px",
+			pt2: "481px",
+			pt3: "641px",
+			ls: "961px", // begins landscape viewports
+			// => @media (min-width: 961px) { ... }
+			ls1: "1025px",
+			ls2: "1281px",
+			ls3: "3840px",
+		},
 		extend: {
 			colors: {
 				"dark-bg": "#040A0E",
