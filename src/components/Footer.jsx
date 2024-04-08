129aee1821757d6407cb7d9368aa9f9980ca621a
Arron-B <arronmail@yahoo.com>
2024-04-08 17:33:35 +0100

    added linkedin to footer and changed github image to an icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer({ isDark }) {
 	const scrollToTop = () => {
@@ -18,29 +19,35 @@ function Footer({ isDark }) {
 					: " bg-light-secondary text-black")
 			}
 		>
-			<p className="text-xs">Arron Bent @2024</p>
+			<p className="text-xs ls:absolute ls:right-1/4">Arron Bent @2024</p>
 			<a href="#welcome">
 				<FontAwesomeIcon
 					className="absolute text-4xl top-[0] left-1/2 -translate-x-1/2 -translate-y-1/2 hover:scale-125"
 					icon={faAngleDoubleUp}
 				/>
 			</a>
-			<a
-				className="h-8 w-8 ml-auto hover:scale-125"
-				href="https://github.com/Arron-B"
-				target="_blank"
-			>
-				<img
-					className={"h-full w-full ml-auto" + (isDark ? " hidden" : "")}
-					src=".\src\assets\github-logo-light.png"
-					alt=""
-				/>
-				<img
-					className={"h-full w-full ml-auto" + (isDark ? "" : " hidden")}
-					src=".\src\assets\github-logo-dark.png"
-					alt=""
-				/>
-			</a>
+			<span className="h-full flex gap-2 absolute right-0 top-1/2 -translate-y-1/2 -translate-x-1/2 items-center ls:left-1/4 ls:right-auto">
+				<a
+					className="h-8 w-8 hover:scale-125"
+					href="https://github.com/Arron-B"
+					target="_blank"
+				>
+					<FontAwesomeIcon
+						className="h-full w-full"
+						icon={faGithub}
+					/>
+				</a>
+				<a
+					className="h-8 w-8 hover:scale-125"
+					href="https://www.linkedin.com/in/arron-bent-939959159/"
+					target="_blank"
+				>
+					<FontAwesomeIcon
+						className="h-8 w-8 hover:scale-125"
+						icon={faLinkedin}
+					/>
+				</a>
+			</span>
 		</div>
 	);
 }
